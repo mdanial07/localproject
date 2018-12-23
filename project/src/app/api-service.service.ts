02340b8724536842f89a30d3bebf04ee12069bf6
mdanial07 <mdanial.md07@gmail.com>
2018-12-23 23:08:01 +0500

    some changes

--- project/src/app/api-service.service.ts
+++ project/src/app/api-service.service.ts
@@ -11,7 +11,7 @@ export class ApiServiceService {
   constructor(private http: HttpClient) { }
 
   retrievePosts(): Observable<any> {
-    return this.http.get<any>(`${environment.url}api/admin/get/posts/`)
+    return this.http.get<any>(`${environment.url}api/admin/get/posts`)
   }
   retrieveManifest(date): Observable<any> {
     return this.http.get<any>(`${environment.url2}api/grainsbazaar/manifest/${date}/`)
