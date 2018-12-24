c46d8025aef53391789ed5024e482a97c040e0cd
behroz <behroz.khan@brainplow.com>
2018-12-24 11:53:43 +0500

    post API consumption

--- project/src/app/Component/NewPost/NewPost.ts
+++ project/src/app/Component/NewPost/NewPost.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'newpost',
@@ -6,6 +8,25 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['../Styles/NewPost.scss']
 })
 export class NewPost implements OnInit {
-    constructor() { }
+    postText
+    btnStatus
+    constructor(private http: HttpClient, private router: Router) { }
     ngOnInit() { }
+    submit() {
+        if (this.postText != "" && this.postText != null && this.postText != undefined) {
+            this.btnStatus = true
+            let obj = {
+                user_id: "1",
+                post_category_id: "1",
+                text: `${this.postText}`
+            }
+            this.http.post("http://grainmarket.qubolt.com/api/admin/post/post/save", obj).subscribe(res => {
+                console.log(res)
+                this.btnStatus = false
+                this.router.navigate(["/"])
+            }, error => {
+                this.btnStatus = false
+            })
+        }
+    }
 }
